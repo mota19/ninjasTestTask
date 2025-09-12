04a6323e250d9286a2d9c7be7bd5132eaff283da
bohdan <mota82589@gmail.com>
2025-09-12 14:30:13 +0300

    delet heroes json

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import heroes from "../Heroes.json" assert { type: "json" };
 
 const app = express();
 const PORT = 5173;
@@ -8,8 +7,9 @@ const PORT = 5173;
 app.use(cors());
 app.use(express.json());
 
-app.get("/getHeroes", (req, res) => {
-  res.json(heroes);
+app.get("/", (req, res) => {
+  console.log("i am working");
+  res.send("i am working");
 });
 
 app.listen(PORT, () => {

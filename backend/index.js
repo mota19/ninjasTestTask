00fa09ca79e6b48cc9ec15f1e235328e38defba5
bohdan <mota82589@gmail.com>
2025-09-12 14:48:02 +0300

    add get heroes

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,19 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: process.env.HOST,
+  port: Number(process.env.PORT),
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.NAME,
+  ssl: { rejectUnauthorized: false },
+});
 
 const app = express();
 const PORT = 5173;
@@ -10,6 +24,16 @@ app.use(express.json());
 app.get("/", (req, res) => {
   console.log("i am working");
   res.send("i am working");
+});
+
+app.get("/heroes", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM heroes");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database error" });
+  }
 });
 
 app.listen(PORT, () => {
